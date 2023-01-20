027f769bdd705d27d25fc39f28ac3e0587119d2e
mohammadtz <mohammadtz1375@gmail.com>
2023-01-20 16:49:38 +0330

    upgrade project to .net 7

--- src/Todo.Infrastructure.Core/Bootstrapper.cs
+++ src/Todo.Infrastructure.Core/Bootstrapper.cs
@@ -1,19 +1,22 @@
 ﻿using Framework.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Todo.Application;
 using Todo.Application.Contract.Tag;
 using Todo.Application.Contract.Todo;
+using Todo.Application.Utils;
 using Todo.Domain.Tag;
 using Todo.Domain.Todo;
 using Todo.Infrastructure.EFCore;
 using Todo.Infrastructure.EFCore.Repositories;
+using ILogger = Todo.Application.Contract.Utils.ILogger;
 
 namespace Todo.Infrastructure.Core;
 
 public static class Bootstrapper
 {
-    public static void Config(this IServiceCollection services, string connectionString)
+    public static void Config(this IServiceCollection services, string? connectionString)
     {
         // applications
         services.AddTransient<ITagApplication, TagApplication>();
@@ -25,8 +28,12 @@ public static class Bootstrapper
 
         // unit of works
         services.AddTransient<IUnitOfWork, UnitOfWorkEf>();
+        
+        services.AddSingleton<ILogger, Logger>();
 
         services.AddDbContext<TodoDbContext>(options =>
-            options.UseLazyLoadingProxies().UseSqlite(connectionString));
+        {
+            if (connectionString != null) options.UseLazyLoadingProxies().UseSqlite(connectionString);
+        });
     }
 }
